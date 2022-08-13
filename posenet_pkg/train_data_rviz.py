6209ee02e3111e705e3a2136187b600aaa20dc75
cwha0212 <cwha02120212@gmail.com>
2022-08-14 00:03:41 +0900

    done

--- posenet_pkg/train_data_rviz.py
+++ posenet_pkg/train_data_rviz.py
@@ -6,15 +6,16 @@ from rclpy.node import Node
 from geometry_msgs.msg import Pose
 from visualization_msgs.msg import Marker, MarkerArray
 
-class RvizMarkerNode(Node):
+class TrainDataRvizNode(Node):
 
   def __init__(self) -> None:
-    super().__init__('rviz_marker_node')
+    super().__init__('train_data_rviz_node')
 
-    self.publisher = self.create_publisher(Marker, 'marker', 10)
+    self.publisher = self.create_publisher(Marker, 'train_marker', 10)
 
-    self.subscriber = self.create_subscription(Pose, 'pose', self.sub_callback, 10)
+    self.subscriber = self.create_subscription(Pose, 'train_pose', self.sub_callback, 10)
 
+    self.num = 0
     self.get_logger().info('Node Started, Waiting.... \n')
 
   def sub_callback(self, msg):
@@ -22,21 +23,28 @@ class RvizMarkerNode(Node):
     marker_msg = Marker()
     image_pose = msg
 
+    marker_msg.header.frame_id = "/my_frame"
     marker_msg.ns = "train_data"
-    marker_msg.id = 1
+    marker_msg.id = self.num
     marker_msg.type = Marker.ARROW
     marker_msg.action = Marker.ADD
+    marker_msg.scale.x = 1.0
+    marker_msg.scale.y = 0.1
+    marker_msg.scale.z = 0.1
+    marker_msg.color.a = 1.0
+    marker_msg.color.r = 0.0
+    marker_msg.color.g = 0.0
+    marker_msg.color.b = 1.0
     marker_msg.pose = image_pose
-    marker_msg.scale = Vector3(2,1,1)
-    marker_msg.color = ColorRGBA(1,0,0,1)
-
+    self.publisher.publish(marker_msg)
+    self.num += 1
 
 def main(args=None):
 
   rclpy.init(args=args)
-  rviz_marker_node = RvizMarkerNode()
+  train_data_rviz_node = TrainDataRvizNode()
 
-  rclpy.spin(rviz_marker_node)
+  rclpy.spin(train_data_rviz_node)
 
   rclpy.shutdown()
   print("done")
