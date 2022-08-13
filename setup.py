dbd5e7b1b7efee55b71938460f710e6dbd812271
cwha0212 <cwha02120212@gmail.com>
2022-08-13 20:58:55 +0900

    test..

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
         'console_scripts': [
             'image_publish_node = posenet_pkg.image_publish:main',
             'Posenet_pytorch_node = posenet_pkg.Posenet_pytorch_node:main',
+            'train_data_publish_node = posenet_pkg.train_data_publish:main',
         ],
     },
 )
